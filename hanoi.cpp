7861b20d24d48ffb21e15aba23a860d8990bfec1
KyinE1 <83590743+KyinE1@users.noreply.github.com>
2021-12-11 10:54:23 -0300

    Update hanoi.cpp

--- hanoi.cpp
+++ hanoi.cpp
@@ -1,9 +1,3 @@
-/*
-	hanoi.cpp
-	Date Written: 1/28
-	Created by: Kyin Edwards
-*/
-
 #include <iostream>
 #include <cstdlib>
 #include <cctype>
@@ -11,54 +5,47 @@
 
 void move_disks(int, int, int, int);
 
-/*
-	Solving the Towers of Hanoi problem using recusive functions.
-	
-	@param argc number of command-line arguements.
-	
-	@param argv command-line arguements.  
-*/
+/**	Solving the Towers of Hanoi problem using recusive functions.
+ *	
+ * @param argc: The number of command-line arguements.
+ * @param argv: The vector containing command-line arguements.  
+ */
 int main(int argc, char* argv[]) {
-	int num_disks;
-	
+	// Error checking for number of arguments.
 	if (argc == 1) {
-		std::cout << "Missing arguement " << std::endl;
-		std::cout << "Usage: hanoi number-of-Disks " << std::endl;
+		std::cout << "Missing arguement.\n";
+		std::cout << "Usage: hanoi number-of-Disks.\n";
 		exit(1);
 	}
 	
 	// Iterative loop to determine if there are only numeric digits.
 	for (int i = 0; argv[1][i] != '\0'; i++)
 		if (!isdigit(argv[1][i])) {
-			std::cout << "Arguement must be a number " << std::endl;
-			std::cout << "Usage: hanoi number-of-Disks " << std::endl;
+			std::cout << "Arguement must be a number.\n";
+			std::cout << "Usage: hanoi number-of-Disks.\n";
 			exit(1);
 		}
 
-	// For numeric string to integer.
-	num_disks = std::stoi(argv[1]);
+	// Numeric string to integer.
+	int num_disks = std::stoi(argv[1]);
 	
-	// Initial call to move_disks().
-	// num_disks from peg 1 to peg 2 while using the temporary holding peg (3).
+	// Initial call to move_disks method.
 	move_disks(num_disks, 1, 2, 3);
 	
 	return 0;
 }
 
-/*
-	Move Disks from starting peg to the next peg using the last peg as a temporary hold. 
-
-	@param num_disks how many Disks available to move.
-	
-	@param src_peg the starting position of the Disks. 
-	
-	@param dest_peg the intended position from the starting peg.
-	
-	@param temp_peg the peg used to hold Disks temporarily. 
-*/
+/**	Move disks from each peg.
+ *
+ * @param num_disks: The number of disks.
+ * @param src_peg: The starting position of the disks. 
+ * @param dest_peg: The intended position from the starting peg.
+ * @param temp_peg: The peg used to hold a disk temporarily. 
+ */
 void move_disks(int num_disks, int src_peg, int dest_peg, int temp_peg) {
+	// Base case.
 	if (num_disks >= 1) {
-		// 1. Move num_disks - 1 Disks from src_peg to temp_peg, using dest_peg to hold.
+		// 1. Move num_disks - 1 disks from src_peg to temp_peg, using dest_peg to hold.
 		move_disks(num_disks - 1, src_peg, dest_peg, temp_peg);
 		
 		// 2. Move last disk (disk # numDisk) from src_peg to dest_peg.
